575f6e1882e2d9a8d0d775e02a64143e1f45da03
Insider <ziglee@mail.ru>
2023-02-13 06:21:33 +0800

    Disabled strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -8,15 +8,13 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        <MantineProvider
-            theme={{ colorScheme: 'dark' }}
-            withGlobalStyles
-            withNormalizeCSS
-        >
-            <App />
-        </MantineProvider>
-    </React.StrictMode>
+    <MantineProvider
+        theme={{ colorScheme: 'dark' }}
+        withGlobalStyles
+        withNormalizeCSS
+    >
+        <App />
+    </MantineProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
