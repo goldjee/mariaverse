7842df664ef99e4d34d6500b48cb35a7b2775980
Insider <ziglee@mail.ru>
2023-02-26 16:54:41 +0800

    Removed unnecessary imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 // import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
-import { comlink } from 'vite-plugin-comlink';
 import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        comlink(),
         ghPages({
             branch: 'gh-pages', // Just ignore the error. It works.
         }),
@@ -22,8 +20,5 @@ export default defineConfig({
     base: '/mariaverse/',
     test: {
         globals: true,
-    },
-    worker: {
-        plugins: [comlink()],
-    },
+    }
 });
